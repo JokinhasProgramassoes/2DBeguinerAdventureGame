7d433e0ee75888e6d92ab4c006bcf1c4d64292c0
JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
2026-03-30 23:22:50 +0100

    Unit - Player Character and movement
    tutorial - create input actions for player character movement;
    tutorial make your game frame rate independent

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class PlayerController : MonoBehaviour
 {
+    //public InputAction LeftAction;
+    public InputAction MoveAction;
+
+    void Start()
+    {
+        //LeftAction.Enable();
+        MoveAction.Enable();
+
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
+    }
+
     void Update()
     {
         //Mover o personagem
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Vector2 position = (Vector2)transform.position + move * 3.0f *  Time.deltaTime;
         transform.position = position;
+
+    
+        //Input horizontal:
+       /* float horizontal = 0.0f;
+        if (LeftAction.IsPressed())
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+
+        //Input Vertical
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+*/
         
+        //position.x = position.x + 0.1f * horizontal;
+        //position.y = position.y + 0.1f * vertical;
     }
 }
