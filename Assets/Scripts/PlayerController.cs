a70d73d059b129a30bd6cabd6937252a67232566
JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
2026-03-31 00:41:27 +0100

    Unit - Game environment and pyshics
    Tutorial - Implement object collisions for your 2D game
    tutorial - Set up tilemap collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,45 +8,27 @@ public class PlayerController : MonoBehaviour
 {
     //public InputAction LeftAction;
     public InputAction MoveAction;
+    Rigidbody2D rb;
+    Vector2 move;
 
     void Start()
     {
         //LeftAction.Enable();
         MoveAction.Enable();
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     void Update()
     {
         //Mover o personagem
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Vector2 position = (Vector2)transform.position + move * 3.0f *  Time.deltaTime;
-        transform.position = position;
-
-    
-        //Input horizontal:
-       /* float horizontal = 0.0f;
-        if (LeftAction.IsPressed())
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-
-        //Input Vertical
-        float vertical = 0.0f;
-        if (Keyboard.current.upArrowKey.isPressed)
-        {
-            vertical = 1.0f;
-        }
-        else if (Keyboard.current.downArrowKey.isPressed)
-        {
-            vertical = -1.0f;
-        }
-*/
+        move = MoveAction.ReadValue<Vector2>();
         
-        //position.x = position.x + 0.1f * horizontal;
-        //position.y = position.y + 0.1f * vertical;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rb.position + move * 3.0f *  Time.deltaTime;
+        rb.position = position;
     }
 }
