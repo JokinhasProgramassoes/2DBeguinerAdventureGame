74259a2025b7565ff138ffb03b94b0d12c370030
JokinhasProgramassoes <joaotomaspereiraribeiro2004@gmail.com>
2026-03-31 01:12:19 +0100

    Unit - Health system
    Tutorial - Set up a basic health system;
    Tutorial - Create a heath collectible using triggers;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 
 public class PlayerController : MonoBehaviour
@@ -10,13 +12,18 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rb;
     Vector2 move;
-
+    public float speed = 3.0f;
+    
+    //Health sistem:
+    public int maxHealth = 5;
+    int currentHealth = 1;
+    
     void Start()
     {
         //LeftAction.Enable();
         MoveAction.Enable();
         rb = GetComponent<Rigidbody2D>();
-
+        //currentHealth = maxHealth;
     }
 
     void Update()
@@ -28,7 +35,13 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rb.position + move * 3.0f *  Time.deltaTime;
+        Vector2 position = (Vector2)rb.position + move * speed *  Time.deltaTime;
         rb.position = position;
+    }
+
+    public void ChangeHealth (int amount)
+    {
+        currentHealth = Math.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
